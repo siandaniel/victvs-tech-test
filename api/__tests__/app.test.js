65ea5099f51c1ab292b98d79984765d80b3f06c1
siandaniel <siandaniel@ymail.com>
2023-04-26 11:03:51 +0100

    add error handler for incorrect path

--- api/__tests__/app.test.js
+++ api/__tests__/app.test.js
@@ -1,6 +1,17 @@
 const request = require("supertest");
 const app = require("../app/app.js");
 
+describe("/non-existent-or-misspelt-endpoint", () => {
+  test("Returns 'Status: 404' and custom error message if endpoint is not found", () => {
+    return request(app)
+      .get("/invalid-path")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not found - this path does not exist");
+      });
+  });
+});
+
 describe("GET /api/data", () => {
   test("Returns 'Status: 200' for successful request", () => {
     return request(app).get("/api/data").expect(200);
--- api/app/app.js
+++ api/app/app.js
@@ -7,4 +7,8 @@ app.use(express.json());
 
 app.get("/api/data", getExams);
 
+app.use((request, response, next) => {
+  response.status(404).send({ msg: "Not found - this path does not exist" });
+});
+
 module.exports = app;
