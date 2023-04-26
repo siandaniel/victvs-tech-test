00924dd8fdf6f6b524d4d68147d75802bef061be
siandaniel <siandaniel@ymail.com>
2023-04-26 11:00:34 +0100

    add error handler to getExams controller

--- api/app/controllers.js
+++ api/app/controllers.js
@@ -1,10 +1,15 @@
 const fetchExamData = require("./models");
 
 const getExams = (request, response, next) => {
-  fetchExamData().then((data) => {
-    const examData = JSON.parse(data);
-    response.status(200).send({ examData });
-  });
+  fetchExamData()
+    .then((data) => {
+      const examData = JSON.parse(data);
+      response.status(200).send({ examData });
+    })
+    .catch((err) => {
+      console.error(err);
+      response.status(500).send("Error reading exam data file");
+    });
 };
 
 module.exports = getExams;
