41f8d45a898e72bca411c0c61419f7c2c531f544
siandaniel <siandaniel@ymail.com>
2023-04-26 15:50:26 +0100

    add basic filter functionality

--- exam-schedule-system-ui/src/components/ExamCard.jsx
+++ exam-schedule-system-ui/src/components/ExamCard.jsx
@@ -1,7 +1,12 @@
 function ExamCard({ exam }) {
   return (
     <li className="exam-card">
-      <p>Exam Card for exam number {exam.id}</p>
+      <h3>{exam.Title}</h3>
+      <p>{exam.Description}</p>
+      <p>{exam.Candidateid}</p>
+      <p>{exam.CandidateName}</p>
+      <p>{exam.Date}</p>
+      <p>{exam.LocationName}</p>
     </li>
   );
 }
--- exam-schedule-system-ui/src/components/ExamList.jsx
+++ exam-schedule-system-ui/src/components/ExamList.jsx
@@ -5,6 +5,8 @@ import { getExamData } from "../utils/api";
 function ExamList() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [filterType, setFilterType] = useState("");
+  const [filterValue, setFilterValue] = useState("");
   const [exams, setExams] = useState([]);
 
   useEffect(() => {
@@ -12,7 +14,7 @@ function ExamList() {
     setError(false);
     getExamData()
       .then((examsFromApi) => {
-        console.log(examsFromApi);
+        console.log(examsFromApi); // Remember to remove later!
         setExams(examsFromApi);
         setLoading(false);
       })
@@ -20,6 +22,35 @@ function ExamList() {
         setError(true);
       });
   }, []);
+
+  function handleFilterTypeChange(event) {
+    setFilterType(event.target.value);
+  }
+
+  function handleFilterValueChange(event) {
+    setFilterValue(event.target.value);
+  }
+
+  function filterExams() {
+    let filteredExams = exams;
+
+    if (filterType === "Candidateid" && filterValue !== "") {
+      filteredExams = filteredExams.filter(
+        (exam) => exam.Candidateid === parseInt(filterValue)
+      );
+    } else if (filterType === "CandidateName" && filterValue !== "") {
+      filteredExams = filteredExams.filter(
+        (exam) => exam.CandidateName.toLowerCase() === filterValue.toLowerCase()
+      );
+    } else if (filterType === "Date" && filterValue !== "") {
+      filteredExams = filteredExams.filter((exam) => exam.Date === filterValue);
+    } else if (filterType === "LocationName" && filterValue !== "") {
+      filteredExams = filteredExams.filter(
+        (exam) => exam.LocationName.toLowerCase() === filterValue.toLowerCase()
+      );
+    }
+    return filteredExams;
+  }
 
   if (loading) {
     return (
@@ -38,8 +69,43 @@ function ExamList() {
   return (
     <div className="exam-list">
       <h2>Exam List</h2>
+      <label htmlFor="filter-type">Filter by:</label>
+      <select
+        id="filter-type"
+        value={filterType}
+        onChange={handleFilterTypeChange}
+      >
+        <option value="">All Exams</option>
+        <option value="Candidateid">Candidate ID</option>
+        <option value="CandidateName">Candidate Name</option>
+        <option value="Date">Date</option>
+        <option value="LocationName">Location</option>
+      </select>
+      {filterType === "Date" && (
+        <>
+          <label htmlFor="filter-value">Value:</label>
+          <p>Date!</p>
+          {/* <input
+            id="filter-value"
+            type="text"
+            value={filterValue}
+            onChange={handleFilterValueChange}
+          /> */}
+        </>
+      )}
+      {filterType !== "" && filterType !== "Date" && (
+        <>
+          <label htmlFor="filter-value">Value:</label>
+          <input
+            id="filter-value"
+            type="text"
+            value={filterValue}
+            onChange={handleFilterValueChange}
+          />
+        </>
+      )}
       <ul>
-        {exams.map((exam, index) => {
+        {filterExams().map((exam) => {
           return <ExamCard exam={exam} key={exam.id} />;
         })}
       </ul>
