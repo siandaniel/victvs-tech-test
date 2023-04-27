ea69f028573d13f42e663237411f361367181308
siandaniel <siandaniel@ymail.com>
2023-04-27 12:31:58 +0100

    restyle exam card border

--- exam-schedule-system-ui/src/components/ExamCard.jsx
+++ exam-schedule-system-ui/src/components/ExamCard.jsx
@@ -1,12 +1,14 @@
 function ExamCard({ exam }) {
   return (
     <li className="exam-card">
+      <div class="exam-details"></div>
       <h2>{exam.Title}</h2>
       <p>{exam.Description}</p>
       <p>{exam.Candidateid}</p>
       <p>{exam.CandidateName}</p>
       <p>{exam.Date}</p>
       <p>{exam.LocationName}</p>
+      <div class="map"></div>
     </li>
   );
 }
